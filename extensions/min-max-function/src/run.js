92b0ff782fa0fe10e6e01e1912a7c39e35094f5e
Neilson Flemming <Neilson.Flemming@Shopify.com>
2024-04-05 18:07:37 -0400

    updated mix-max function to use reduce function

--- extensions/min-max-function/src/run.js
+++ extensions/min-max-function/src/run.js
@@ -11,40 +11,29 @@
  */
 
 export function run(input) {
-  const errors = [];
-
-  input.cart.lines.forEach((lineItem) => {
+  const { lines } = input.cart;
+  const errors = lines.reduce((acc, lineItem) => {
     const { quantity, merchandise } = lineItem;
     const maxOrderQuantity = Number.parseInt(merchandise?.maxorderquantity?.value);
     const minOrderQuantity = Number.parseInt(merchandise?.minorderquantity?.value);
 
-    console.log(
-      "Product Title",
-      merchandise?.product?.title,
-      "Variant Title",
-      merchandise?.title,
-      "- Cart Quantity:",
-      quantity,
-      "- Min Order Quantity:",
-      minOrderQuantity,
-      "- Max Order Quantity:",
-      maxOrderQuantity
-    );
+    console.log("Product Title: ", merchandise?.product?.title, " Variant Title: ",merchandise?.title,
+    " Cart Quantity: ", quantity, " Min Order Quantity:", minOrderQuantity, " Max Order Quantity:", maxOrderQuantity);
 
-    // Comapre cart quantity to max and min order quantity
+    // Compare cart quantity to max and min order quantity
     if (quantity > maxOrderQuantity) {
-      errors.push({
+      acc.push({
         localizedMessage: `Sorry, the maximum order quantity for ${merchandise?.product?.title} / ${merchandise?.title} is ${maxOrderQuantity}`,
         target: "cart",
       });
     } else if (quantity < minOrderQuantity) {
-      errors.push({
+      acc.push({
         localizedMessage: `Sorry, the minimum order quantity for ${merchandise?.product?.title} / ${merchandise?.title} is ${minOrderQuantity}`,
         target: "cart",
       });
-    } else {
-      console.log("Quantity is within limits");
     }
-  });
+    return acc;
+  }, []);
+
   return { errors };
 }
